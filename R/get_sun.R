6b799a9527aa3eedbc6cf92a42845c837d0536c4
Adam Smith <avianmigration@gmail.com>
2018-03-28 13:52:01 +0000

    Can now request multiple ephemerides, incl. civil rise and set (dawn and dusk)

--- R/get_sun.R
+++ R/get_sun.R
@@ -7,8 +7,10 @@
 #' @param lat numeric scalar of position latitude (decimal degrees; WGS84)
 #' @param start character or Date scalar of start date ("YYYY-MM-DD")
 #' @param end character or Date scalar of start date ("YYYY-MM-DD")
-#' @param direction one of "dawn", "dusk", "sunrise", or "sunset", indicating
-#'   which ephemerides should be calculated
+#' @param direction any combination of "dawn", "dusk", "sunrise", or "sunset",
+#'  indicating which ephemerides should be calculated. Here, dawn and dusk
+#'  represent civil sunrise and civil sunset (sun 6 degrees below horizon),
+#'  respectively.
 #' @param out_tz time zone specification to be used for output. System-specific
 #'   (see \link[base]{timezones}. Default is "GMT" (UTC; Universal Time,
 #'   Coordinated).
@@ -16,25 +18,35 @@
 #' @examples
 #' get_sun(-83, 34)
 #' get_sun(-83, 34, start = "2017-12-31", end = "2017-12-31",
-#'         direction = "sunset", out_tz = "America/New_York")
+#'         direction = c("sunrise", "sunset"), out_tz = "America/New_York")
 
 get_sun <- function (lon, lat, start = Sys.Date(),
-                     end = start, direction = c("sunrise", "sunset"),
+                     end = start,
+                     direction = c("sunrise", "sunset", "dawn", "dusk"),
                      out_tz = "GMT") {
+
+  stopifnot(all(direction %in% c("sunrise", "sunset", "dawn", "dusk")))
 
   if (!requireNamespace("maptools", quietly = TRUE))
     install.packages("maptools", quiet = TRUE)
 
-  direction <- match.arg(direction)
   ll <- cbind(lon, lat)
   start <- as.POSIXct(start, tz = "GMT")
   end <- as.POSIXct(end, tz = "GMT")
   sequence <- seq(from = start, to = end, by = "days")
-  sun <- maptools::sunriset(ll, sequence, direction = direction, POSIXct = TRUE)$time
-  if (out_tz != "GMT") {
-    attributes(sun)$tzone <- out_tz
-  }
-  sun <- data.frame(date_str = as.character(sequence), sun, stringsAsFactors = FALSE)
-  names(sun)[2] <- direction
+
+  sun <- lapply(direction, function(d) {
+    if (d %in% c("sunrise", "sunset"))
+      tmp <- maptools::sunriset(ll, sequence, direction = d, POSIXct = TRUE)$time
+    else
+      tmp <- maptools::crepuscule(ll, sequence, solarDep = 6, direction = d, POSIXct = TRUE)$time
+    if (out_tz != "GMT") {
+      attributes(tmp)$tzone <- out_tz
+    }
+    tmp <- data.frame(date_str = as.character(sequence), tmp, stringsAsFactors = FALSE)
+    names(tmp)[2] <- d
+    tmp
+  })
+  sun <- Reduce(function(x, y) merge(x, y, by = "date_str", all = TRUE), sun)
   sun
 }
