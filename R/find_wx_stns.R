60e8cfe23573c9ce2585a0856e40ff966a979815
Adam Smith <avianmigration@gmail.com>
2018-04-04 16:27:30 +0000

    Add option to search for stations by address

--- R/find_wx_stns.R
+++ R/find_wx_stns.R
@@ -4,6 +4,9 @@
 #'
 #' @param lon numeric scalar of position longitude (decimal degrees; WGS84)
 #' @param lat numeric scalar of position latitude (decimal degrees; WGS84)
+#' @param address character scalar of a street address or place name
+#'  (e.g. "Mattamuskeet NWR" or "135 Phoenix Rd, Athens, GA"); overrides
+#'  \code{lat} and \code{lon} if specified. See example
 #' @param n_stns how many nearest stations to return?
 #' @param id_only logical; return only station abbreviations or station info too?
 #' @param plot logical; generate plot showing station positions relative to input
@@ -12,13 +15,27 @@
 #' @examples
 #' find_wx_stns(-83, 34)
 #' find_wx_stns(-83, 34, id_only = FALSE)
+#' \dontrun{
+#' find_wx_stns(address = "Mattamuskeet NWR", id_only = FALSE, plot = TRUE)
+#' }
 
-find_wx_stns <- function(lon, lat, n_stns = 5, id_only = TRUE, plot = FALSE) {
+find_wx_stns <- function(lon = NULL, lat = NULL, address = NULL,
+                         n_stns = 5, id_only = TRUE, plot = FALSE) {
 
   if (!requireNamespace("geosphere", quietly = TRUE))
     install.packages("geosphere", quiet = TRUE)
 
-  stopifnot(all(c(!is.na(lat), !is.na(lon))))
+  if (!is.null(address)) {
+    if (!requireNamespace("ggmap", quietly = TRUE))
+      install.packages("ggmap", quiet = TRUE)
+    if (!is.character(address))
+      stop('`address` must be a character scalar of a street address ',
+           'or place name (e.g. "Mattamuskeet NWR" or "135 Phoenix Rd, Athens, GA")')
+    ll <- suppressMessages(ggmap::geocode(address))
+    lat <- ll$lat; lon <- ll$lon
+  }
+
+  stopifnot(all(c(!is.null(lat), !is.null(lon))))
   stopifnot(identical(length(lat), length(lon)))
   stns <- readRDS(system.file("extdata/wx_stations.rds", package = "nrsmisc"))
   ll <- cbind(lon, lat)
@@ -41,9 +58,13 @@ find_wx_stns <- function(lon, lat, n_stns = 5, id_only = TRUE, plot = FALSE) {
     latr <- latr + c(-1, 1) * 0.5 * diff(latr)
     lonr <- range(plot_df$lon)
     lonr <- lonr + c(-1, 1) * 0.5 * diff(lonr)
-    suppressMessages(suppressWarnings(
-      bm <- ggmap::get_map(location = c(lonr[1], latr[1], lonr[2], latr[2]))
-    ))
+    bm <- try(suppressWarnings(
+                suppressMessages(
+                  bm <- ggmap::get_map(location = c(lonr[1], latr[1], lonr[2], latr[2])))),
+              silent = TRUE)
+    if (inherits(bm, "try-error"))
+      warning("Google Maps API failure; skipping plot. Maybe wait a minute and try again.")
+    else {
       bm <- ggmap::ggmap(bm) +
         ggplot2::geom_point(data = plot_df[plot_df$id == "INPUT", ],
                             ggplot2::aes(x = lon, y = lat),
@@ -55,6 +76,7 @@ find_wx_stns <- function(lon, lat, n_stns = 5, id_only = TRUE, plot = FALSE) {
                             size=2, nudge_y = diff(latr)/35,
                             label.padding = ggplot2::unit(0.15, "lines"))
       plot(bm)
+    }
   }
 
   if (id_only) out <- out$id
